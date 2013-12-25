8b9193ef648223fd31887901c6ca84bad9202b08
Alexander Libster <alexander.libster@naturalint.com>
2013-12-25 19:31:33 +0200

    upload ni-tree

---
+++ demo/javascript/index.js
@@ -0,0 +1,261 @@
+var treeData1 = firstTreeData();
+var treeData2 = secondTreeData();
+
+var tree1 = $('#tree1').niTree({treeData: treeData1})
+    ,$log1 = $('#log1');
+$('#btn_expand1').on('click', function(){
+    tree1.niTree('expandAll');
+});
+$('#btn_collapse1').on('click', function(){
+    tree1.niTree('collapseAll');
+});
+$('#btn_checked1').on('click', function(){
+    $log1.prepend('[' + tree1.niTree('get', {selected: true}).join(', ') + ']\n');
+});
+$('#btn_checked_leafs1').on('click', function(){
+    $log1.prepend('[' + tree1.niTree('get', {selected: true, leafsOnly: true}).join(', ') + ']\n');
+});
+$('#btn_unchecked1').on('click', function(){
+    $log1.prepend('[' + tree1.niTree('get', {selected: false}).join(', ') + ']\n');
+});
+$('#btn_unchecked_leafs1').on('click', function(){
+    $log1.prepend('[' + tree1.niTree('get', {selected: false, leafsOnly: true}).join(', ') + ']\n');
+});
+
+var tree2 = $('#tree2').niTree({treeData: treeData2})
+    ,$log2 = $('#log2');
+$('#btn_expand2').on('click', function(){
+    tree2.niTree('expandAll');
+});
+$('#btn_collapse2').on('click', function(){
+    tree2.niTree('collapseAll');
+});
+$('#btn_checked2').on('click', function(){
+    $log2.prepend('[' + tree2.niTree('get', {selected: true}).join(', ') + ']\n');
+});
+$('#btn_checked_leafs2').on('click', function(){
+    $log2.prepend('[' + tree2.niTree('get', {selected: true, leafsOnly: true}).join(', ') + ']\n');
+});
+$('#btn_unchecked2').on('click', function(){
+    $log2.prepend('[' + tree2.niTree('get', {selected: false}).join(', ') + ']\n');
+});
+$('#btn_unchecked_leafs2').on('click', function(){
+    $log2.prepend('[' + tree2.niTree('get', {selected: false, leafsOnly: true}).join(', ') + ']\n');
+});
+
+
+function firstTreeData(){
+    return [
+        {
+            label: 'Node0 (html enabled: <a href="#">link tag</a>)',
+            id: 'node0',
+            attributes: {
+                'class': ['class_node0_a', 'class_node0_b', 'red_node'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'Node0_1',
+                    id: 'node0_1',
+                    attributes: {
+                        'class': ['class_node0_1_a', 'class_node0_1_b', 'bold'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node0_1_1',
+                            id: 'node0_1_1',
+                            attributes: {
+                                'class': ['class_node0_1_1_a', 'class_node0_1_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Node1',
+            id: 'node1',
+            attributes: {
+                'class': ['class_node1_a', 'class_node1_b'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'Node1_1',
+                    id: 'node1_1',
+                    attributes: {
+                        'class': ['class_node1_1_a', 'class_node1_1_b', 'red_node'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_1_1',
+                            id: 'node1_1_1',
+                            attributes: {
+                                'class': ['class_node1_1_1_a', 'class_node1_1_1_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_1_2',
+                            id: 'node1_1_2',
+                            attributes: {
+                                'class': ['class_node1_1_2_a', 'class_node1_1_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_2',
+                    id: 'node1_2',
+                    attributes: {
+                        'class': ['class_node1_2_a', 'class_node1_2_b', 'ni_tree_init_checked'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_2_1',
+                            id: 'node1_2_1',
+                            attributes: {
+                                'class': ['class_node1_2_1_a', 'class_node1_2_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_2_2',
+                            id: 'node1_2_2',
+                            attributes: {
+                                'class': ['class_node1_2_2_a', 'class_node1_2_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_3',
+                    id: 'node1_3',
+                    attributes: {
+                        'class': ['class_node1_3_a'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_3_1',
+                            id: 'node1_3_1',
+                            attributes: {
+                                'class': ['class_node1_3_1_a', 'class_node1_3_1_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_3_3',
+                            id: 'node1_3_3',
+                            attributes: {
+                                'class': ['class_node1_3_2_a', 'class_node1_3_2_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+}
+
+function secondTreeData(){
+    return [
+        {
+            label: 'Node0 (html enabled: <i>italic</i>)',
+            id: 'node0',
+            attributes: {
+                'class': ['class_node0_a', 'class_node0_b', 'red_node'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'Node0_1',
+                    id: 'node0_1',
+                    attributes: {
+                        'class': ['class_node0_1_a', 'class_node0_1_b', 'bold'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node0_1_1',
+                            id: 'node0_1_1',
+                            attributes: {
+                                'class': ['class_node0_1_1_a', 'class_node0_1_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            label: 'Node1',
+            id: 'node1',
+            attributes: {
+                'class': ['class_node1_a', 'class_node1_b'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'Node1_1',
+                    id: 'node1_1',
+                    attributes: {
+                        'class': ['class_node1_1_a', 'class_node1_1_b', 'red_node'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_1_1',
+                            id: 'node1_1_1',
+                            attributes: {
+                                'class': ['class_node1_1_1_a', 'class_node1_1_1_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_1_2',
+                            id: 'node1_1_2',
+                            attributes: {
+                                'class': ['class_node1_1_2_a', 'class_node1_1_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_2',
+                    id: 'node1_2',
+                    attributes: {
+                        'class': ['class_node1_2_a', 'class_node1_2_b', 'ni_tree_init_checked'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_2_1',
+                            id: 'node1_2_1',
+                            attributes: {
+                                'class': ['class_node1_2_1_a', 'class_node1_2_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_2_2',
+                            id: 'node1_2_2',
+                            attributes: {
+                                'class': ['class_node1_2_2_a', 'class_node1_2_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ];
+}
---
+++ jquery.nitree.js
@@ -0,0 +1,129 @@
+(function($){
+    var methods = {
+        init : function(options) {
+            var defaults = {
+                treeData: [{'label': 'Empty tree'}]
+            };
+            options = $.extend(defaults, options);
+            function addNodes($el, treeData, root){
+                treeData.forEach(function(child){
+                    child.attributes = child.attributes || {};
+                    $.each(child.attributes, function(key, value){
+                        child.attributes[key] = $.isArray(value) ? value.join(' ') : value;
+                    });
+                    if(child.id){
+                        child.attributes.id = child.id;
+                    };
+                    var $ul = $('<ul>', root ? {'class': 'ni_tree'} : {}),
+                        $li = $('<li>', child.attributes),
+                        $label = $('<span/>').html(child.label);
+                    $li.append($label);
+                    $ul.append($li);
+                    $el.append($ul);
+                    child.children && addNodes($li, child.children);
+                });
+            }
+            function init_tree_open_close($tree){
+                //add controls
+                var $li = $tree.find('li'),
+                    li_attr = {'type': 'checkbox', 'class': 'tree_checkbox'};
+                $tree.find('li')
+                    .filter(':not(.ni_tree_no_checkbox)')
+                    .prepend($('<input>', li_attr))
+                    .end()
+                    .has('ul')
+                    .prepend($('<span>', {'class': 'tree_ctrl'}))
+                    .parent().addClass('has_subtree');
+                //open/close functionality for the tree
+                $tree.find('.tree_ctrl').click(function(){
+                    $(this).parent()
+                        .toggleClass('tree_close');
+                });
+            }
+
+            //disable the checkboxes of the empty lists (empty = doesn't have li-children with checkboxes)
+            function disable_checkboxes_for_empty_lists($tree){
+                var $empty_lists = $tree.find('li')
+                    .has('li')
+                    .filter(':not(:has(li .tree_checkbox))');
+                $empty_lists.children('.tree_checkbox').prop('disabled', true);
+                $empty_lists.children('.tree_checkbox')
+                    .removeClass('tree_checkbox')
+                    .addClass('tree_checkbox_disabled');
+            }
+
+            function init_checking_behavior($tree){
+                $tree.find('.tree_checkbox').change(function(){
+                    var checked = $(this).is(':checked');
+                    $(this).parent().find('.tree_checkbox')
+                        .prop('checked', checked)
+                        .prop('indeterminate', false);
+
+                    $(this).prop("indeterminate", false);
+                    $(this).parents('li')
+                        .each(function(){
+                            var $container_li = $(this).parents('li:first');
+                            var $containers_checkboxes = $container_li.children('input.tree_checkbox');
+                            $containers_checkboxes.prop("indeterminate", false);
+                            if ($container_li.find('li input.tree_checkbox:not(:checked)').length) {
+                                $containers_checkboxes.prop('checked', false);
+                                if ($container_li.find('li input.tree_checkbox:checked').length) {
+                                    $containers_checkboxes.prop("indeterminate", true);
+                                }
+                            } else {
+                                $containers_checkboxes.prop('checked', true);
+                            }
+                        });
+                });
+                $tree.find('li.ni_tree_init_checked>.tree_checkbox')
+                    .prop('checked', true)
+                    .change();
+            }
+            return this.each(function() {
+                addNodes($(this), options.treeData, true);
+
+                init_tree_open_close($(this));
+                disable_checkboxes_for_empty_lists($(this));
+                init_checking_behavior($(this));
+            })
+        },
+        //options example: {selected: false, leafsOnly: true} - get unselected leafs
+        get: function(options){
+            var defaults = {selected: true};
+            options = $.extend(defaults, options);
+            var $lis = this;
+            if(options.leafsOnly){
+                $lis = this.find('li:not(:has(ul))');
+            };
+            var $treeCheckboxes;
+            if(options.selected){
+                $treeCheckboxes = $lis.find('.tree_checkbox:checked');
+            } else {
+                $treeCheckboxes = $lis.find('.tree_checkbox:not(:checked)');
+            };
+            return $treeCheckboxes.map(function(){
+                return $(this).parent().prop('id');
+            }).toArray();
+        },
+        expandAll : function() {
+            this.find('.has_subtree').children('li').removeClass('tree_close');
+            return this;
+        },
+        collapseAll : function() {
+            this.find('.has_subtree').children('li').addClass('tree_close');
+            return this;
+        }
+    };
+
+    $.fn.niTree = function(methodOrOptions) {
+        if(methods[methodOrOptions] ){
+            return methods[ methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof methodOrOptions === 'object' || ! methodOrOptions) {
+            // Default to "init"
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' +  methodOrOptions + ' does not exist on jQuery.niTree');
+        }
+    };
+
+})(jQuery);
