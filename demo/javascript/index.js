80493fe88e344253ccae23461acc58d35409d282
AlexanderLibs <Alexander.Libster@naturalint.com>
2013-12-31 14:14:59 +0200

    demo for "append subtree" functionality

--- demo/javascript/index.js
+++ demo/javascript/index.js
@@ -1,8 +1,11 @@
 var treeData1 = firstTreeData();
 var treeData2 = secondTreeData();
+var subTreeData = subTreeData();
 
 var tree1 = $('#tree1').niTree({treeData: treeData1})
     ,$log1 = $('#log1');
+tree1.niTree({treeData: subTreeData, parentNodeID: 'tree1_node0_1'})
+
 $('#btn_expand1').on('click', function(){
     tree1.niTree('expandAll');
 });
@@ -48,7 +51,7 @@ function firstTreeData(){
     return [
         {
             label: 'Node0 (html enabled: <a href="#">link tag</a>)',
-            id: 'node0',
+            id: 'tree1_node0',
             attributes: {
                 'class': ['class_node0_a', 'class_node0_b', 'red_node'],
                 'data-type': 'site_brand_list'
@@ -56,15 +59,144 @@ function firstTreeData(){
             children: [
                 {
                     label: 'Node0_1',
-                    id: 'node0_1',
+                    id: 'tree1_node0_1',
+                    attributes: {
+                        'class': ['class_node0_1_a', 'class_node0_1_b', 'bold'],
+                        'data-type': 'site_brand_list_child'
+                    }/*,
+                    children: [
+                        {
+                            label: 'Node0_1_1',
+                            id: 'tree1_node0_1_1',
+                            attributes: {
+                                'class': ['class_node0_1_1_a', 'class_node0_1_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]*/
+                }
+            ]
+        },
+        {
+            label: 'Node1',
+            id: 'tree1_node1',
+            attributes: {
+                'class': ['class_node1_a', 'class_node1_b'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'Node1_1',
+                    id: 'tree1_node1_1',
+                    attributes: {
+                        'class': ['class_node1_1_a', 'class_node1_1_b', 'red_node'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_1_1',
+                            id: 'tree1_node1_1_1',
+                            attributes: {
+                                'class': ['class_node1_1_1_a', 'class_node1_1_1_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_1_2',
+                            id: 'tree1_node1_1_2',
+                            attributes: {
+                                'class': ['class_node1_1_2_a', 'class_node1_1_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_2',
+                    id: 'tree1_node1_2',
+                    attributes: {
+                        'class': ['class_node1_2_a', 'class_node1_2_b', 'ni_tree_init_checked'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_2_1',
+                            id: 'tree1_node1_2_1',
+                            attributes: {
+                                'class': ['class_node1_2_1_a', 'class_node1_2_1_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_2_2',
+                            id: 'tree1_node1_2_2',
+                            attributes: {
+                                'class': ['class_node1_2_2_a', 'class_node1_2_2_b'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_3',
+                    id: 'tree1_node1_3',
+                    attributes: {
+                        'class': ['class_node1_3_a'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                    children: [
+                        {
+                            label: 'Node1_3_1',
+                            id: 'tree1_node1_3_1',
+                            attributes: {
+                                'class': ['class_node1_3_1_a', 'class_node1_3_1_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        },
+                        {
+                            label: 'Node1_3_3',
+                            id: 'tree1_node1_3_3',
+                            attributes: {
+                                'class': ['class_node1_3_2_a', 'class_node1_3_2_b', 'ni_tree_no_checkbox'],
+                                'data-type': 'site_brand_list_child'
+                            }
+                        }
+                    ]
+                },
+                {
+                    label: 'Node1_4',
+                    id: 'tree1_node1_4',
+                    attributes: {
+                        'class': ['class_node1_4_a'],
+                        'data-type': 'site_brand_list_child'
+                    },
+                }
+            ]
+        }
+    ];
+}
+
+function secondTreeData(){
+    return [
+        {
+            label: 'tree2_Node0 (html enabled: <i>italic</i>)',
+            id: 'tree2_node0',
+            attributes: {
+                'class': ['class_node0_a', 'class_node0_b', 'red_node'],
+                'data-type': 'site_brand_list'
+            },
+            children: [
+                {
+                    label: 'tree2_Node0_1',
+                    id: 'tree2_node0_1',
                     attributes: {
                         'class': ['class_node0_1_a', 'class_node0_1_b', 'bold'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node0_1_1',
-                            id: 'node0_1_1',
+                            label: 'tree2_Node0_1_1',
+                            id: 'tree2_node0_1_1',
                             attributes: {
                                 'class': ['class_node0_1_1_a', 'class_node0_1_1_b'],
                                 'data-type': 'site_brand_list_child'
@@ -75,32 +207,32 @@ function firstTreeData(){
             ]
         },
         {
-            label: 'Node1',
-            id: 'node1',
+            label: 'tree2_Node1',
+            id: 'tree2_node1',
             attributes: {
                 'class': ['class_node1_a', 'class_node1_b'],
                 'data-type': 'site_brand_list'
             },
             children: [
                 {
-                    label: 'Node1_1',
-                    id: 'node1_1',
+                    label: 'tree2_Node1_1',
+                    id: 'tree2_node1_1',
                     attributes: {
                         'class': ['class_node1_1_a', 'class_node1_1_b', 'red_node'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node1_1_1',
-                            id: 'node1_1_1',
+                            label: 'tree2_Node1_1_1',
+                            id: 'tree2_node1_1_1',
                             attributes: {
                                 'class': ['class_node1_1_1_a', 'class_node1_1_1_b', 'ni_tree_no_checkbox'],
                                 'data-type': 'site_brand_list_child'
                             }
                         },
                         {
-                            label: 'Node1_1_2',
-                            id: 'node1_1_2',
+                            label: 'tree2_Node1_1_2',
+                            id: 'tree2_node1_1_2',
                             attributes: {
                                 'class': ['class_node1_1_2_a', 'class_node1_1_2_b'],
                                 'data-type': 'site_brand_list_child'
@@ -109,52 +241,26 @@ function firstTreeData(){
                     ]
                 },
                 {
-                    label: 'Node1_2',
-                    id: 'node1_2',
+                    label: 'tree2_Node1_2',
+                    id: 'tree2_node1_2',
                     attributes: {
                         'class': ['class_node1_2_a', 'class_node1_2_b', 'ni_tree_init_checked'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node1_2_1',
-                            id: 'node1_2_1',
+                            label: 'tree2_Node1_2_1',
+                            id: 'tree2_node1_2_1',
                             attributes: {
                                 'class': ['class_node1_2_1_a', 'class_node1_2_1_b'],
                                 'data-type': 'site_brand_list_child'
                             }
                         },
                         {
-                            label: 'Node1_2_2',
-                            id: 'node1_2_2',
+                            label: 'tree2_Node1_2_2',
+                            id: 'tree2_node1_2_2',
                             attributes: {
                                 'class': ['class_node1_2_2_a', 'class_node1_2_2_b'],
-                                'data-type': 'site_brand_list_child'
-                            }
-                        }
-                    ]
-                },
-                {
-                    label: 'Node1_3',
-                    id: 'node1_3',
-                    attributes: {
-                        'class': ['class_node1_3_a'],
-                        'data-type': 'site_brand_list_child'
-                    },
-                    children: [
-                        {
-                            label: 'Node1_3_1',
-                            id: 'node1_3_1',
-                            attributes: {
-                                'class': ['class_node1_3_1_a', 'class_node1_3_1_b', 'ni_tree_no_checkbox'],
-                                'data-type': 'site_brand_list_child'
-                            }
-                        },
-                        {
-                            label: 'Node1_3_3',
-                            id: 'node1_3_3',
-                            attributes: {
-                                'class': ['class_node1_3_2_a', 'class_node1_3_2_b', 'ni_tree_no_checkbox'],
                                 'data-type': 'site_brand_list_child'
                             }
                         }
@@ -165,27 +271,27 @@ function firstTreeData(){
     ];
 }
 
-function secondTreeData(){
+function subTreeData(){
     return [
         {
-            label: 'Node0 (html enabled: <i>italic</i>)',
-            id: 'node0',
+            label: 'sub_tree_Node0 (html enabled: <i>italic</i>)',
+            id: 'sub_tree_node0',
             attributes: {
                 'class': ['class_node0_a', 'class_node0_b', 'red_node'],
                 'data-type': 'site_brand_list'
             },
             children: [
                 {
-                    label: 'Node0_1',
-                    id: 'node0_1',
+                    label: 'sub_tree_Node0_1',
+                    id: 'sub_tree_node0_1',
                     attributes: {
                         'class': ['class_node0_1_a', 'class_node0_1_b', 'bold'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node0_1_1',
-                            id: 'node0_1_1',
+                            label: 'sub_tree_Node0_1_1',
+                            id: 'sub_tree_node0_1_1',
                             attributes: {
                                 'class': ['class_node0_1_1_a', 'class_node0_1_1_b'],
                                 'data-type': 'site_brand_list_child'
@@ -196,58 +302,58 @@ function secondTreeData(){
             ]
         },
         {
-            label: 'Node1',
-            id: 'node1',
+            label: 'sub_tree_Node1',
+            id: 'sub_tree_node1',
             attributes: {
                 'class': ['class_node1_a', 'class_node1_b'],
                 'data-type': 'site_brand_list'
             },
             children: [
                 {
-                    label: 'Node1_1',
-                    id: 'node1_1',
+                    label: 'sub_tree_Node1_1',
+                    id: 'sub_tree_node1_1',
                     attributes: {
                         'class': ['class_node1_1_a', 'class_node1_1_b', 'red_node'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node1_1_1',
-                            id: 'node1_1_1',
+                            label: 'sub_tree_Node1_1_1',
+                            id: 'sub_tree_node1_1_1',
                             attributes: {
                                 'class': ['class_node1_1_1_a', 'class_node1_1_1_b', 'ni_tree_no_checkbox'],
                                 'data-type': 'site_brand_list_child'
                             }
                         },
                         {
-                            label: 'Node1_1_2',
-                            id: 'node1_1_2',
+                            label: 'sub_tree_Node1_1_2',
+                            id: 'sub_tree_node1_1_2',
                             attributes: {
-                                'class': ['class_node1_1_2_a', 'class_node1_1_2_b'],
+                                'class': ['class_node1_1_2_a', 'class_node1_1_2_b', 'ni_tree_no_checkbox'],
                                 'data-type': 'site_brand_list_child'
                             }
                         }
                     ]
                 },
                 {
-                    label: 'Node1_2',
-                    id: 'node1_2',
+                    label: 'sub_tree_Node1_2',
+                    id: 'sub_tree_node1_2',
                     attributes: {
                         'class': ['class_node1_2_a', 'class_node1_2_b', 'ni_tree_init_checked'],
                         'data-type': 'site_brand_list_child'
                     },
                     children: [
                         {
-                            label: 'Node1_2_1',
-                            id: 'node1_2_1',
+                            label: 'sub_tree_Node1_2_1',
+                            id: 'sub_tree_node1_2_1',
                             attributes: {
                                 'class': ['class_node1_2_1_a', 'class_node1_2_1_b'],
                                 'data-type': 'site_brand_list_child'
                             }
                         },
                         {
-                            label: 'Node1_2_2',
-                            id: 'node1_2_2',
+                            label: 'sub_tree_Node1_2_2',
+                            id: 'sub_tree_node1_2_2',
                             attributes: {
                                 'class': ['class_node1_2_2_a', 'class_node1_2_2_b'],
                                 'data-type': 'site_brand_list_child'
